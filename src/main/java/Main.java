bc1d3d75407387ef4c136d1f17aeeb83f4e6e430
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2019-09-12 10:16:02 +0300

    why console is empty?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,22 @@
 import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Observable.just("Hello world!")
-                .subscribe(a -> System.out.println(a));
+        /*Observable.just("Hello world!")
+                .subscribe(a -> System.out.println(a));*/
 
+        Observable.range(1, 10)
+                .subscribeOn(Schedulers.computation())
+                .subscribe(Main::print);
+    }
+
+    private static void print(int i) throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread name = " + Thread.currentThread().getName());
+        System.out.println(i);
     }
 }
