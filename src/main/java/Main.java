edc5495891600e5eef5d5a714792aa3ee33fff44
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2019-09-19 10:14:36 +0300

    add parallelism

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,21 +2,34 @@ import io.reactivex.Observable;
 import io.reactivex.Scheduler;
 import io.reactivex.schedulers.Schedulers;
 
-import java.util.ArrayList;
-
 public class Main {
-    public static void main(String[] args) {
-        /*Observable.just("Hello world!")
-                .subscribe(a -> System.out.println(a));*/
+    public static void main(String[] args) throws InterruptedException {
+        Scheduler computation = Schedulers.computation();
 
         Observable.range(1, 10)
-                .subscribeOn(Schedulers.computation())
+                .flatMap(integer -> Observable.just(integer)
+                        .map(Main::myDouble)
+                        .subscribeOn(computation)
+                )
+//                .map(Main::myDouble)
+//                .subscribeOn(computation)
                 .subscribe(Main::print);
+
+        Thread.sleep(12000);
+//        Thread.currentThread().join();
     }
 
     private static void print(int i) throws InterruptedException {
+//        Thread.currentThread().join();
         Thread.sleep(1000);
         System.out.println("Thread name = " + Thread.currentThread().getName());
         System.out.println(i);
+    }
+
+    private static int myDouble(int i) throws InterruptedException {
+//        Thread.currentThread().join();
+        Thread.sleep(1000);
+        System.out.println("MyDouble: Thread name = " + Thread.currentThread().getName());
+        return i * 2;
     }
 }
